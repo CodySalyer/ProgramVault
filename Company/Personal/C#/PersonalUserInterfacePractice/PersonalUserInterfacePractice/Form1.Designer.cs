5cc58fcb276abdb9ac5c056c09a2bc6b2b0c1155
Cody Salyer <codysalyer@live.com>
2016-06-06 15:13:02 -0600

    Creation of C# test UI interface, this will be used to test and archive code pieces

---
+++ Company/Personal/C#/PersonalUserInterfacePractice/PersonalUserInterfacePractice/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PersonalUserInterfacePractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.buttonBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(12, 12);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(126, 23);
+            this.buttonTest.TabIndex = 0;
+            this.buttonTest.Text = "TestButtonText";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // buttonBox
+            // 
+            this.buttonBox.Location = new System.Drawing.Point(145, 13);
+            this.buttonBox.Multiline = true;
+            this.buttonBox.Name = "buttonBox";
+            this.buttonBox.Size = new System.Drawing.Size(100, 20);
+            this.buttonBox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear Text Box";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonBox);
+            this.Controls.Add(this.buttonTest);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.TextBox buttonBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
